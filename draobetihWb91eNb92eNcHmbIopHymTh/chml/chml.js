167804da712c03c33b31deaec1c4273863daba26
otgreviewer <41872550+otgreviewer@users.noreply.github.com>
2018-07-30 00:43:39 +0800

    OtgrevV1

---
+++ draobetihWb91eNb92eNcHmbIopHymTh/chml/chml.js
@@ -0,0 +1,39 @@
+
+               	 	$("#uprotons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of protons = 92</h3><p>Because the number of protons is equivalent to the atomic number of the given element (Uranium) which has an atomic number of 92.</p></div>');
+
+					});
+               	 
+               	 	$("#uelectrons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of electrons = 92</h3><p>Because the number of electrons is equivalent to the atomic number of the given element (Uranium) which has an atomic number of 92.</p></div>');
+
+					});
+               	 
+               	 	$("#uneutrons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of neutrons = 143</h3><p>Because the number of neutrons is equivalent to the mass number (235) the minus atomic number (92).<br>235 - 92 = 143.</p></div>');
+
+					});
+               	 
+               	 	$("#siprotons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of protons = 14</h3><p>Because the number of protons is equivalent to the atomic number of the given element (Uranium) which has an atomic number of 14.</p></div>');
+
+					});
+               	 
+               	 	$("#silectrons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of electrons = 14</h3><p>Because the number of electrons is equivalent to the atomic number of the given element (Uranium) which has an atomic number of 14.</p></div>');
+
+					});
+               	 
+               	 	$("#sineutrons").on('click', function() {
+
+ 					 $.fancybox.open('<div class="message"><h3>Number of neutrons = 16</h3><p>Because the number of neutrons is equivalent to the mass number (30) the minus atomic number (14).<br>30 - 14 = 16.</p></div>');
+
+					});
+
+
+      
